9390d5a088aa5ca6bb332d480ff9dc29eccee7f9
aknysh <andriy.knysh@gmail.com>
2018-03-06 18:42:08 -0500

    Update config

--- rootfs/usr/bin/teleport-auth-connectors.sh
+++ rootfs/usr/bin/teleport-auth-connectors.sh
@@ -2,6 +2,6 @@
 
 gomplate -f /etc/teleport/templates/teleport-github-connector-template.yaml -o /etc/teleport/teleport-github-connector.yaml
 
-until
-    tctl create -f /etc/teleport/teleport-github-connector.yaml; do echo 'retrying teleport-github-connector...' && sleep 1;
+until tctl create -f /etc/teleport/teleport-github-connector.yaml; do
+    echo 'retrying teleport-github-connector...' && sleep 1;
 done
